29ed25f51793ba3ccb598ca00c68ee373040fd7e
Pushpendra Kumar <sainipushpendra51@gmail.com>
2022-01-02 17:34:51 +0530

    Update AppDelegate.swift

--- GMapPractice/AppDelegate.swift
+++ GMapPractice/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyD9nrBDq0IPeAa8_9-oxT9pla9nm8yZk20")
+        GMSServices.provideAPIKey("MY-GOOGLE_MAP-KEY")
         return true
     }
 
